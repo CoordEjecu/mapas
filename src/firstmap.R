1b5fccef9d45c089f8664bad4bd3b6093c0411c4
paco chavez <francisco.chavez@sonora.gob.mx>
2025-04-10 15:52:56 -0700

    Se grafico 2 shp

--- src/firstmap.R
+++ src/firstmap.R
@@ -2,14 +2,16 @@ library(sf)
 library(ggplot2)
 
 # Read the shapefile
-shape_data <- st_read("/workdir/data/shp/DISTRITO_FEDERAL.shp")
+distritos <- st_read("/workdir/data/shp/DISTRITO_FEDERAL.shp")
+manzanas <- st_read("/workdir/data/shp/MANZANA.shp")
 
 # Basic plot
 plot(shape_data)
 
 # Nicer plot with ggplot2
 ggplot() +
-  geom_sf(data = shape_data) +
+  geom_sf(data = distritos) +
+  geom_sf(data = manzanas, fill = "blue", color = "darkblue", alpha = 0.5) +
   ggtitle("My Shapefile Map") +
   theme_minimal()
 ggsave("borrame.png")
