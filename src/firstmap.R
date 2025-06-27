44f2d3503c492e2b87c15af14f2a39ed4fc9dfcd
Nepo Rojas <nepo.rojas@gmail.com>
2025-06-27 15:57:59 -0700

    🚧 agrega nuevas capas

--- src/firstmap.R
+++ src/firstmap.R
@@ -2,14 +2,20 @@ library(sf)
 library(ggplot2)
 
 # Read the shapefile
-distritos <- st_read("/workdir/data/26 SONORA/DISTRITO_FEDERAL.shp")
-manzanas <- st_read("/workdir/data/26 SONORA/MANZANA.shp")
+distritos <- st_read("/workdir/data/26 SONORA/DISTRITO_FEDERAL.shp") |>
+  dplyr::filter(DISTRITO == 4)
+manzanas <- st_read("/workdir/data/26 SONORA/FERROCARRIL.shp")
+municipios <- st_read("/workdir/data/26 SONORA/MUNICIPIO.shp") |>
+  dplyr::filter(NOMBRE == "HERMOSILLO")
+escuelas <- st_read("/workdir/data/26 SONORA/ESCUELA.shp")
 
 
 # Nicer plot with ggplot2
 ggplot() +
   geom_sf(data = distritos) +
   geom_sf(data = manzanas, fill = "blue", color = "darkblue", alpha = 0.5) +
+  geom_sf(data = municipios, fill = "yellow", color = "darkblue") +
+  geom_sf(data = escuelas, color = "red", size = 2) +
   ggtitle("My Shapefile Map") +
   theme_minimal()
 ggsave("borrame.png")
