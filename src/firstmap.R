c976e152da0d14c20c57a0681ab38f685332fff3
Nepo Rojas <nepo.rojas@gmail.com>
2025-06-30 13:17:04 -0700

    🎨 Format with Styler

--- src/firstmap.R
+++ src/firstmap.R
@@ -10,19 +10,20 @@ escuelas <- st_read("/workdir/data/26 SONORA/ESCUELA.shp")
 
 porcentajes <- readr::read_csv("/workdir/data/REPORTE_AFILIACION_04_MUN.csv", show_col_types = FALSE) |>
   dplyr::mutate(
-  porc = 100* AVANCE/META,
-  categoria = dplyr::case_when(
-    porc >= 90 ~ "90-100 (Verde)",
-    porc >= 50 ~ "50-89 (Naranja)",
-    TRUE ~ "0-49 (Rojo)"
-  ))
+    porc = 100 * AVANCE / META,
+    categoria = dplyr::case_when(
+      porc >= 90 ~ "90-100 (Verde)",
+      porc >= 50 ~ "50-89 (Naranja)",
+      TRUE ~ "0-49 (Rojo)"
+    )
+  )
 
 colores <- c(
-  "0-49 (Rojo)" = "#FF0000",     # Rojo
+  "0-49 (Rojo)" = "#FF0000", # Rojo
   "50-89 (Naranja)" = "#FFA500", # Naranja
-  "90-100 (Verde)" = "#00FF00"   # Verde
+  "90-100 (Verde)" = "#00FF00" # Verde
 )
-completos <- porcentajes |> 
+completos <- porcentajes |>
   dplyr::left_join(municipios, by = c("MUNICIPIO" = "NOMBRE"))
 # Nicer plot with ggplot2
 ggplot() +
