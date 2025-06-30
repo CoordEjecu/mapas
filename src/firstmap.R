49a993d7e57d34f42cc9faffd74b741a84e50497
Nepo Rojas <nepo.rojas@gmail.com>
2025-06-30 13:14:52 -0700

    🚧 Add map with progress

--- src/firstmap.R
+++ src/firstmap.R
@@ -5,11 +5,25 @@ library(ggplot2)
 distritos <- st_read("/workdir/data/26 SONORA/DISTRITO_FEDERAL.shp") |>
   dplyr::filter(DISTRITO == 4)
 manzanas <- st_read("/workdir/data/26 SONORA/FERROCARRIL.shp")
-municipios <- st_read("/workdir/data/26 SONORA/MUNICIPIO.shp") |>
-  dplyr::filter(NOMBRE == "HERMOSILLO")
+municipios <- st_read("/workdir/data/26 SONORA/MUNICIPIO.shp")
 escuelas <- st_read("/workdir/data/26 SONORA/ESCUELA.shp")
 
+porcentajes <- readr::read_csv("/workdir/data/REPORTE_AFILIACION_04_MUN.csv", show_col_types = FALSE) |>
+  dplyr::mutate(
+  porc = 100* AVANCE/META,
+  categoria = dplyr::case_when(
+    porc >= 90 ~ "90-100 (Verde)",
+    porc >= 50 ~ "50-89 (Naranja)",
+    TRUE ~ "0-49 (Rojo)"
+  ))
 
+colores <- c(
+  "0-49 (Rojo)" = "#FF0000",     # Rojo
+  "50-89 (Naranja)" = "#FFA500", # Naranja
+  "90-100 (Verde)" = "#00FF00"   # Verde
+)
+completos <- porcentajes |> 
+  dplyr::left_join(municipios, by = c("MUNICIPIO" = "NOMBRE"))
 # Nicer plot with ggplot2
 ggplot() +
   geom_sf(data = distritos) +
@@ -19,3 +33,15 @@ ggplot() +
   ggtitle("My Shapefile Map") +
   theme_minimal()
 ggsave("borrame.png")
+
+ggplot(completos) +
+  geom_sf(data = municipios) +
+  geom_sf(aes(geometry = geometry, fill = categoria)) +
+  scale_fill_manual(
+    name = "Rangos",
+    values = colores,
+    drop = FALSE
+  ) +
+  theme_minimal() +
+  labs(title = "Municipios por rangos de valor")
+ggsave("/workdir/borrame_cc.png")
