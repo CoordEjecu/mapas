75e7591a23cc6f84bfb1f6ba1a30475f393fb67e
Nepo Rojas <nepo.rojas@gmail.com>
2025-07-08 10:39:52 -0700

    🎨 Format with Styler

--- src/firstmap.R
+++ src/firstmap.R
@@ -53,7 +53,7 @@ ggplot(completos) +
 ggsave("/workdir/avance_de_brigadeo.png")
 
 etiquetas_opt |>
-  dplyr::select(MUNICIPIOS, ID, lon, lat, distancia_a_hillo,tiempo) |>
+  dplyr::select(MUNICIPIOS, ID, lon, lat, distancia_a_hillo, tiempo) |>
   dplyr::filter(!is.na(ID)) |>
   dplyr::arrange(tiempo) |>
   readr::write_csv("/workdir/etiquetas_optimizadas.csv")
