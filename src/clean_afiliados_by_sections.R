602afa8d8cd4afea77bacda04bbc445eda1cc77c
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-04 12:06:24 -0700

    ✨ Add new script to clean data

---
+++ src/clean_afiliados_by_sections.R
@@ -0,0 +1,13 @@
+datos <- readr::read_csv(
+    "/workdir/data/REPORTE_AFILIACION_04_X_SECCION_20250731.csv",
+    show_col_types = FALSE,
+    col_types = list(META = readr::col_integer(), AVANCE = readr::col_integer()))
+
+municipio <- datos |>
+  dplyr::group_by(MUNICIPIO) |>
+  dplyr::summarise(
+    meta = sum(META, na.rm = TRUE),
+    avance = sum(AVANCE, na.rm = TRUE),
+    porcentaje = avance / meta * 100,
+    .groups = "drop"
+  )
