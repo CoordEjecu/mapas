a4f76ad1ca53c6eee2932540e184f1732fb8dade
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-13 13:14:32 -0700

    🚧 Add differeny from Moctezuma

--- src/resource_radios.R
+++ src/resource_radios.R
@@ -24,11 +24,13 @@ time_and_distance_moctezuma <- readr::read_csv("/workdir/data/distancias_desde_m
   select_municipy_and_resource()
 
 to_compare_resource <- time_and_distance_hillo |>
-  dplyr::left_join(time_and_distance_guaymas, by = c("MUNICIPIO")) |>
+  dplyr::left_join(time_and_distance_guaymas, by = c("MUNICIPIO"), suffix = c("_hillo", "_guay")) |>
   dplyr::left_join(time_and_distance_moctezuma, by = c("MUNICIPIO")) |>
   dplyr::mutate(
-    diff_guaymas_hillo = recursos.y - recursos.x,
-    diff_mocte_hillo = recursos - recursos.x,
-    better_from_hillo = ifelse((diff_guaymas_hillo > -120) & (diff_mocte_hillo > -240), TRUE, FALSE)
+    diff_guaymas_hillo = recursos_guay - recursos_hillo,
+    diff_mocte_hillo = recursos - recursos_hillo,
+    diff_guaymas_mocte = recursos_guay - recursos,
+    better_from_hillo = ifelse((diff_guaymas_hillo > -120) & (diff_mocte_hillo > -240), TRUE, FALSE),
+    better_from_mocte = ifelse(!(diff_mocte_hillo > -120) & (diff_guaymas_mocte > -440), TRUE, FALSE)
   ) |>
   readdp::write_csv("/workdir/data/better_from_hillo.csv")
