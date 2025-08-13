34ee34738f0a9fa2ed05c47b0f3fddbe3073b4c2
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-13 13:17:52 -0700

    🚧✅ Change name of template to `mapas`

--- tests/testthat/test_nothing.R
+++ tests/testthat/test_nothing.R
@@ -9,7 +9,7 @@ describe("Test all is ready", {
 describe("Get version of the module", {
   it("The version is 0.0.1", {
     expected_version <- c("0.0.1")
-    obtained_version <- packageVersion("templater")
+    obtained_version <- packageVersion("mapas")
     version_are_equal <- expected_version == obtained_version
     expect_true(version_are_equal)
   })
