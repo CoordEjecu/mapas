7762914d5c571e7910edf6e5c75b4d8a14d6de9d
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-19 12:07:23 -0700

    🚧 Add version to write info about municipies

---
+++ src/mapa_con_referencias.R
@@ -0,0 +1,48 @@
+library(sf)
+library(ggplot2)
+
+id_name = jsonlite::fromJSON("tests/data/id_name_municipies.json")
+municipio <- id_name$BACUM
+# Read the shapefile
+municipios <- sf::st_read("/workdir/data/26 SONORA/MUNICIPIO.shp") |>
+  dplyr::filter(MUNICIPIO == municipio)
+cabeceras <- st_read("/workdir/data/26 SONORA/CABECERA_MUNICIPAL.shp") |>
+  dplyr::filter(MUNICIPIO == municipio)
+secciones <- st_read("/workdir/data/26 SONORA/SECCION.shp") |>
+  dplyr::filter(MUNICIPIO == municipio)
+id_seccion <- secciones$SECCION
+cabecera <- stringr::str_to_lower(stringr::str_replace_all(cabeceras$LOCALIDAD_.1, " ", "_"))
+sections_with_votes_path <- glue::glue("/workdir/results/sections_with_votes_{cabecera}.csv")
+sections_with_votes <- readr::read_csv("/workdir/data/sections_with_votes.csv", show_col_types = FALSE) |>
+  dplyr::filter(SECCION %in% id_seccion) |>
+  readdp::write_csv(sections_with_votes_path)
+
+municipy_map <- glue::glue("/workdir/results/secciones_de_{cabecera}.png")
+map_municipy <- ggplot() +
+  geom_sf(data = municipios) +
+  geom_sf(data = secciones) +
+  geom_sf(
+    data = cabeceras,
+    shape = "★", # Símbolo de estrella (unicode)
+    color = "black", # Color dorado
+    size = 10, # Tamaño
+    stroke = 1.5 # Grosor del borde (opcional)
+  ) +
+  theme_minimal() +
+  labs(title = glue::glue("{stringr::str_to_title(cabeceras$LOCALIDAD_.1)}"))
+
+entidad <- st_read("/workdir/data/26 SONORA/ENTIDAD.shp")
+el_mapita <- ggplot() +
+  geom_sf(data = entidad) +
+  geom_sf(data = municipios, fill = "black") +
+  theme_void()
+
+mapa_final <- cowplot::ggdraw() +
+  cowplot::draw_plot(map_municipy) +
+  cowplot::draw_plot(
+    el_mapita,
+    x = 0.05, y = 0.55, # Posición del inset (ajusta según necesidad)
+    width = 0.3, height = 0.3 # Tamaño del inset
+  )
+
+ggsave(municipy_map, plot = mapa_final)
---
+++ src/write_municipies_and_sections_paragraph.R
@@ -0,0 +1,43 @@
+library(sf)
+
+id_name = jsonlite::fromJSON("tests/data/id_name_municipies.json")
+municipio <- id_name$BACUM
+cabeceras <- st_read("/workdir/data/26 SONORA/CABECERA_MUNICIPAL.shp") |>
+  dplyr::filter(MUNICIPIO == municipio)
+secciones <- st_read("/workdir/data/26 SONORA/SECCION.shp") |>
+  dplyr::filter(MUNICIPIO == municipio)
+id_seccion <- secciones$SECCION
+
+name_head_municipy <- cabeceras$LOCALIDAD_.1 |>
+  stringr::str_replace_all(" ", "_") |>
+  stringr::str_to_lower()
+goals_and_affiliate <- readr::read_csv("/workdir/data/REPORTE_AFILIACION_04_X_SECCION.csv", show_col_types = FALSE) |>
+  dplyr::select(c(4, 6:8))
+sections_with_votes_path <- glue::glue("/workdir/results/sections_with_votes_{name_head_municipy}.csv")
+sections_with_votes <- readr::read_csv("/workdir/data/sections_with_votes.csv", show_col_types = FALSE) |>
+  dplyr::filter(SECCION %in% id_seccion) |>
+  dplyr::left_join(goals_and_affiliate, by = c("SECCION" = "SECCION")) |>
+  dplyr::relocate(nominal, .after = SECCION) |>
+  readr::write_csv(sections_with_votes_path)
+
+make_list_from_row <- function(row) {
+  list(
+    municipio = stringr::str_to_title(row$MUNICIPIO),
+    distancia = row$distancia,
+    tiempo = row$tiempo,
+    meta = row$meta,
+    avance = row$avance,
+    municipy_path = name_head_municipy,
+    all_sections = id_seccion
+  )
+}
+
+time_and_distance_path <- glue::glue("/workdir/data/non-tabular/time_and_distance_{name_head_municipy}.json")
+time_and_distance <- readr::read_csv("/workdir/data/time_and_distance.csv", show_col_types = FALSE)
+goals_and_votes <- readr::read_csv("/workdir/data/goals_and_votes.csv", show_col_types = FALSE)
+time_and_distance |>
+  dplyr::left_join(goals_and_votes, by = c("MUNICIPIO" = "NOMBRE_MUNICIPIO")) |>
+  dplyr::filter(MUNICIPIO == cabeceras$LOCALIDAD_.1) |>
+  make_list_from_row() |>
+  jsonlite::toJSON(pretty = T, force = TRUE, auto_unbox = TRUE) |>
+  write(time_and_distance_path)
