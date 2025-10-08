3f39449fe52e12634921f6c98475240f642e8d83
Nepo Rojas <nepo.rojas@gmail.com>
2025-10-08 14:40:22 -0700

    🔥 Delete section part

--- src/votes_by_sections_in_distrito.R
+++ src/votes_by_sections_in_distrito.R
@@ -1,7 +1,6 @@
 library(sf)
 library(ggplot2)
 
-
 secciones <- st_read("/workdir/data/26 SONORA/SECCION.shp") |>
   dplyr::filter(DISTRITO == 4) |>
   dplyr::pull(SECCION)
@@ -10,17 +9,6 @@ ayuntamiento <- readr::read_csv("data/AYUNTAMIENTO_2021.csv", show_col_types = F
   dplyr::filter(seccion %in% secciones)
 
 total_de_votos_efectivos <- sum(ayuntamiento$total_votos, na.rm = TRUE)
-
-sections_with_40 <- ayuntamiento |>
-  dplyr::group_by(seccion) |>
-  dplyr::summarise(
-	total = sum(total_votos, na.rm = TRUE),
-	.groups = "drop"
-  ) |>
-  dplyr::arrange(-total) |>
-  dplyr::mutate(acumulado = cumsum(total)) |>
-  dplyr::filter(acumulado <= total_de_votos_efectivos * 0.4) |>
-  dplyr::pull(seccion)
 
 name_municipies_with_80 <- ayuntamiento |>
   dplyr::group_by(municipio) |>
@@ -40,7 +28,6 @@ municipios <- sf::st_read("/workdir/data/26 SONORA/MUNICIPIO.shp") |>
     lon = st_coordinates(centroide)[, 1],
     lat = st_coordinates(centroide)[, 2]
   )
-
 
 entidad <- st_read("/workdir/data/26 SONORA/ENTIDAD.shp")
 color_pantone <- "#9D2449"
