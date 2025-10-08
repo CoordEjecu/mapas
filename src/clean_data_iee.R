939a43b0a635b733cdb4beb7dc347e976b8fba1a
Nepo Rojas <nepo.rojas@gmail.com>
2025-10-08 14:45:45 -0700

    🎨 Format with Styler

--- src/clean_data_iee.R
+++ src/clean_data_iee.R
@@ -2,18 +2,21 @@ year <- 2024
 path <- glue::glue("/workdir/data/AYUNTAMIENTO_{year}.csv")
 datos <- readr::read_csv(
   path,
-  show_col_types = FALSE)
+  show_col_types = FALSE
+)
 
 party_name <- "MOVIMIENTO_CIUDADANO"
 party <- datos |>
   dplyr::group_by(seccion) |>
   dplyr::summarize(
-	partido = sum(!!rlang::sym(party_name)),
-	nominal = sum(lista_nominal),
-	total = sum(`total_votos`)) |>
+    partido = sum(!!rlang::sym(party_name)),
+    nominal = sum(lista_nominal),
+    total = sum(`total_votos`)
+  ) |>
   dplyr::mutate(
-	porcentaje_votos = partido / nominal * 100,
-	participacion_ciudadana = total / nominal * 100) |>
+    porcentaje_votos = partido / nominal * 100,
+    participacion_ciudadana = total / nominal * 100
+  ) |>
   dplyr::ungroup() |>
   dplyr::arrange(-total)
 
