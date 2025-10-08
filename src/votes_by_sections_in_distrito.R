3b807ea41174e3f9a77edcc7bb964fd6c37e7316
Nepo Rojas <nepo.rojas@gmail.com>
2025-10-08 14:24:46 -0700

    🚧 Add new script.
    Calcula los municipio con los que se alcaza el 80 de los votos efectivos y las secciones con el 40 de los votos efectivos.

---
+++ src/votes_by_sections_in_distrito.R
@@ -0,0 +1,61 @@
+library(sf)
+library(ggplot2)
+
+
+secciones <- st_read("/workdir/data/26 SONORA/SECCION.shp") |>
+  dplyr::filter(DISTRITO == 4) |>
+  dplyr::pull(SECCION)
+
+ayuntamiento <- readr::read_csv("data/AYUNTAMIENTO_2021.csv", show_col_types = FALSE) |>
+  dplyr::filter(seccion %in% secciones)
+
+total_de_votos_efectivos <- sum(ayuntamiento$total_votos, na.rm = TRUE)
+
+sections_with_40 <- ayuntamiento |>
+  dplyr::group_by(seccion) |>
+  dplyr::summarise(
+	total = sum(total_votos, na.rm = TRUE),
+	.groups = "drop"
+  ) |>
+  dplyr::arrange(-total) |>
+  dplyr::mutate(acumulado = cumsum(total)) |>
+  dplyr::filter(acumulado <= total_de_votos_efectivos * 0.4) |>
+  dplyr::pull(seccion)
+
+name_municipies_with_80 <- ayuntamiento |>
+  dplyr::group_by(municipio) |>
+  dplyr::summarise(
+	total = sum(total_votos, na.rm = TRUE),
+	.groups = "drop"
+  ) |>
+  dplyr::arrange(-total) |>
+  dplyr::mutate(acumulado = cumsum(total)) |>
+  dplyr::filter(acumulado <= total_de_votos_efectivos * 0.8) |>
+  dplyr::pull(municipio)
+
+municipios <- sf::st_read("/workdir/data/26 SONORA/MUNICIPIO.shp") |>
+  dplyr::filter(NOMBRE %in% name_municipies_with_80) |>
+  dplyr::mutate(
+    centroide = st_centroid(geometry),
+    lon = st_coordinates(centroide)[, 1],
+    lat = st_coordinates(centroide)[, 2]
+  )
+
+
+entidad <- st_read("/workdir/data/26 SONORA/ENTIDAD.shp")
+color_pantone <- "#9D2449"
+el_mapita <- ggplot() +
+  geom_sf(data = entidad) +
+  geom_sf(data = municipios, fill = color_pantone) +
+  geom_text(
+    data = municipios,
+    aes(x = lon, y = lat, label = MUNICIPIO),
+    size = 2,
+    color = "black",
+    fontface = "bold"
+  ) +
+  theme_minimal()
+
+municipy_map <- glue::glue("/workdir/results/municipios_copn_el_80_de_votos.png")
+
+ggsave(el_mapita, filename = municipy_map, width = 8, height = 6)
